6179ec1a20557657068669e7bf4cbd624c5fe6f6
diegosc1512 <42757319+diegosc1512@users.noreply.github.com>
2022-08-27 21:50:46 -0400

    Update demo.js
    modificando archivo demo.js

--- demo.js
+++ demo.js
@@ -3,3 +3,5 @@ console.log("Hola clase");
 console.log("Diplomado Fullstack");
 console.log("Hola clase dia 6 - 25/08/2022");
 console.log("Diplomado Fullstack");
+console.log("Buenos noches clase dia 6 - 25/08/2022");
+console.log("Diplomado Fullstack");
